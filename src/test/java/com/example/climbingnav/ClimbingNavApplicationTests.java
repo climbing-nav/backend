2319eef6368c00fa1043ad168bbdcb1ca577393c
신관호 <sin99145@naver.com>
2025-08-11 10:17:43 +0900

    initial

---
+++ src/test/java/com/example/climbingnav/ClimbingNavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climbingnav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingNavApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
